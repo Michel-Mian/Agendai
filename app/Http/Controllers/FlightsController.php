d38767ff1b2eb1f3e50ba08d994617d5278a9732
Rafael Fante <augusto.fante3360@gmail.com>
2025-06-24 18:34:13 -0300

    creation of advanced filters modal

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -24,11 +24,15 @@ class FlightsController extends Controller
             'hl' => 'pt-br',
             'travel_class' => $request->class,
             'api_key'     => $apiKey,
+            'stops' => $request->stops,
+            'outbound_times' => $this->timesToHoursString($request->departure_time),
+            'return_times' => $this->timesToHoursString($request->arrival_time),
+            'exclude_airlines' => is_array($request->exclude_airlines) ? implode(',', $request->exclude_airlines) : $request->exclude_airlines,
             
         ];
         //dd($params);
         $response = Http::get('https://serpapi.com/search', $params);
-        //dd($response->json());
+        dd($response->json());
         $json = $response->json();
 
         $flights = [];
@@ -49,7 +53,77 @@ class FlightsController extends Controller
             ['path' => url()->current(), 'query' => $request->query()]
         );
 
-        return view('flights', ['flights' => $paginator, 'title' => 'Voos']);
+       $airlines = [
+            // América Latina
+            'LATAM'                 => 'LA',
+            'Gol'                   => 'G3',
+            'Azul'                  => 'AD',
+            'Avianca'               => 'AV',
+            'Aeromexico'            => 'AM',
+            'Copa Airlines'         => 'CM',
+            'Sky Airline'           => 'H2',
+            'Jetsmart'              => 'JA',
+            
+            // América do Norte
+            'American Airlines'     => 'AA',
+            'AMERICAN'              => 'AA',
+            'Delta'                 => 'DL',
+            'United Airlines'       => 'UA',
+            'United'                => 'UA',
+            'Air Canada'            => 'AC',
+            'WestJet'               => 'WS',
+            'Alaska Airlines'       => 'AS',
+            'JetBlue'               => 'B6',
+            'Southwest Airlines'    => 'WN',
+            'Spirit Airlines'       => 'NK',
+            'Frontier Airlines'     => 'F9',
+
+            // Europa
+            'Lufthansa'             => 'LH',
+            'British Airways'       => 'BA',
+            'Air France'            => 'AF',
+            'KLM'                   => 'KL',
+            'Iberia'                => 'IB',
+            'Ryanair'               => 'FR',
+            'EasyJet'               => 'U2',
+            'Swiss International'   => 'LX',
+            'Turkish Airlines'      => 'TK',
+            'TAP Air Portugal'      => 'TP',
+            'Alitalia'              => 'AZ',
+            'Norwegian Air Shuttle' => 'DY',
+            'Wizz Air'              => 'W6',
+            'Brussels Airlines'     => 'SN',
+            'Aer Lingus'            => 'EI',
+
+            // Ásia
+            'Emirates'              => 'EK',
+            'Qatar Airways'         => 'QR',
+            'Etihad Airways'        => 'EY',
+            'Singapore Airlines'    => 'SQ',
+            'Cathay Pacific'        => 'CX',
+            'ANA (All Nippon Airways)' => 'NH',
+            'Japan Airlines'        => 'JL',
+            'Thai Airways'          => 'TG',
+            'Malaysia Airlines'     => 'MH',
+            'Korean Air'            => 'KE',
+            'Asiana Airlines'       => 'OZ',
+            'IndiGo'                => '6E',
+            'Air India'             => 'AI',
+            'Air China'             => 'CA',
+            'China Southern'        => 'CZ',
+            'China Eastern'         => 'MU',
+
+            // Oceania
+            'Qantas'                => 'QF',
+            'Air New Zealand'       => 'NZ',
+
+            // África
+            'Ethiopian Airlines'    => 'ET',
+            'South African Airways' => 'SA',
+            'EgyptAir'              => 'MS',
+            'Royal Air Maroc'       => 'AT',
+        ];
+        return view('flights', ['flights' => $paginator, 'title' => 'Voos', 'airlines' => $airlines]);
     }
 
     private function paginateArray(array $items, $perPage = 6, $page = null, $options = [])
@@ -65,5 +139,19 @@ class FlightsController extends Controller
             $page,
             $options
         );
+    }
+
+    // Função auxiliar para converter horários para string de horas separadas por vírgula
+    private function timesToHoursString($times)
+    {
+        if (is_array($times)) {
+            $hours = array_map(function($t) {
+                return intval(explode(':', $t)[0]);
+            }, $times);
+            return implode(',', $hours);
+        } elseif ($times) {
+            return intval(explode(':', $times)[0]);
+        }
+        return null;
     }
 }
