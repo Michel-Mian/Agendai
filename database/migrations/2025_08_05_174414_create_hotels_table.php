e7e4eb15a1f28e1f2d7d57239ae2c38860d0cba3
Michel mian <michelmian11@gmail.com>
2025-08-08 17:55:23 -0300

    start page hotels

---
+++ database/migrations/2025_08_05_174414_create_hotels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id('pk_id_hotel');
+            $table->string('nome_hotel')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->decimal('avaliacao', 2, 1)->nullable();
+            $table->date('data_check_in')->nullable();
+            $table->date('data_check_out')->nullable();
+            $table->string('preco', 100)->nullable();
+            $table->string('image_url')->nullable();
+            $table->unsignedBigInteger('fk_id_viagem');
+            $table->foreign('fk_id_viagem')->references('pk_id_viagem')->on('viagens')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
